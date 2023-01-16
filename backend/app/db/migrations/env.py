738f411c4bc13c3b4f6e0ebe5561f8a215eb345c
Abhinav Kumar <abhinav.vikash2@gmail.com>
2023-01-17 04:03:17 +0530

    Added is Verified@7

--- backend/app/db/migrations/env.py
+++ backend/app/db/migrations/env.py
@@ -5,9 +5,9 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[3]))
 
+from app.core.config import get_app_settings  # isort:skip
 
 SETTINGS = get_app_settings()
 
