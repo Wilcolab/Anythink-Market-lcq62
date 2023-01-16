6616a50ccb792ea40b2422b9b2b57b98388e5f0c
Abhinav Kumar <abhinav.vikash2@gmail.com>
2023-01-16 20:50:45 +0000

    Added is_veified.

---
+++ backend/app/db/migrations/versions/4baabf026e0b_added_is_verified_to_user_schema.py
@@ -0,0 +1,23 @@
+"""Added is_verified to User Schema.
+
+Revision ID: 4baabf026e0b
+Revises: fdf8821871d7
+Create Date: 2023-01-16 20:49:12.001081
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '4baabf026e0b'
+down_revision = 'fdf8821871d7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
